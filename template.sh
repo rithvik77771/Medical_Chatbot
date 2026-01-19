5b6f1341b740bbeb26f2d6010263e6910db3e583
Rithvik Pallikonda <rithvikpallikonda@192.168.1.7>
2026-01-19 13:56:37 +0530

    adding folders and files

---
+++ template.sh
@@ -0,0 +1,16 @@
+# create directory
+mkdir -p src
+mkdir -p research
+
+#create files
+touch src/__init__.py
+touch src/helper.py
+touch src/prompt.py
+touch .env
+touch requirements.txt
+touch setup.py
+touch app.py
+touch research/test.ipynb
+
+
+echo "setup complete"
