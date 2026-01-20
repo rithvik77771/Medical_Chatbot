4db130a35d6408ef4f6ea9e89a65dc80dd3dade2
Rithvik Pallikonda <rithvikpallikonda@Rithviks-MacBook-Air.local>
2026-01-20 10:25:42 +0530

    adding requirments

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Medical_Chatbot
+
+
+
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup,find_packages
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="rithvik",
+    author_email="pallikondarithvik@gmail.com",
+    packages=find_packages(),
+    install_requires=[
+    ],
+)
